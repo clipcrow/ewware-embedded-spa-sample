908350c62f49ea9d6778552ee2e5b600d58d802e
masataka <m@clipcrow.com>
2024-08-03 12:05:27 +0900

    refactoring

--- serve.ts
+++ serve.ts
@@ -3,14 +3,16 @@ import { Application } from "oak";
 const app = new Application();
 
 app.use(async (ctx) => {
-  const secret = ctx.request.headers.get("X-Workware-Signature");
+  const { headers, url: { searchParams } } = ctx.request;
+  const secret = headers.get("X-Workware-Signature");
   if (secret !== "essential-workware") {
     ctx.response.status = 401;
     ctx.response.body = `401 Unauthorized. ${secret}`;
     return;
   };
-  console.log(ctx.request.headers)
-  console.log(ctx.request.url.searchParams);  
+  console.log(`CORS: ${headers.get("access-control-allow-origin")}`);
+  console.log(`Workspace UUID: ${searchParams.get("workspace")}`);  
+  console.log(`User UUID: ${searchParams.get("user")}`);  
   try {
     await ctx.send({
       root: `${Deno.cwd()}/public`,
