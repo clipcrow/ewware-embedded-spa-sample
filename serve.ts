1a930cbcb316abaa2ea4a9196905d30bdece115c
danh-ld <danh-ld@genkisystem.com>
2025-07-23 11:14:34 +0700

    Add additional signature validation for unauthorized access

--- serve.ts
+++ serve.ts
@@ -5,7 +5,8 @@ const app = new Application();
 app.use(async (ctx) => {
   const { headers, url: { searchParams } } = ctx.request;
   const secret = headers.get("X-ClipCrow-Signature");
-  if (secret !== "clipcrow") {
+  const secretInReq = searchParams.get("X-ClipCrow-Signature");
+  if (secret !== "clipcrow" && secretInReq !== "clipcrow") {
     ctx.response.status = 401;
     ctx.response.body = `401 Unauthorized. ${secret}`;
     return;
